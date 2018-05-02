84a8b13a8bd98d9d809ae937805642a410330b92
Gennadiy Khatuntsev <e.steelcat@gmail.com>
2018-05-02 09:37:12 +0500

    Added export accounts

--- src/providers/ability-cash/XmlBuilder.ts
+++ src/providers/ability-cash/XmlBuilder.ts
@@ -37,6 +37,28 @@ export class XmlBuilder {
             }
         }
 
+        let accounts = fd.getAccounts();
+        if (accounts.length > 0) {
+            let section = root.ele('accounts');
+
+            for (let a of accounts) {
+                let account = section.ele('account');
+                let currency = fd.getCurrency(a.currencyId);
+
+                account.ele('name', a.title);
+                account.ele('init-balance', 0);
+                // @todo: comment
+
+                if (currency) {
+                    account.ele('currency', prepareCode(currency.name));
+                }
+
+                if (!a.isActive) {
+                    account.ele('locked');
+                }
+            }
+        }
+
         return root.end({
             pretty: true,
             indent: ' '.padStart(4)
