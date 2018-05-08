499e040a6467695b4d7e4c73e56ec3ca5841c860
Gennadiy Khatuntsev <e.steelcat@gmail.com>
2018-05-08 15:06:09 +0500

    Removed unused code

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,6 @@ import { AbilityCashProvider } from './providers/ability-cash/AbilityCashProvide
 import { FinancistoProvider } from './providers/financisto/FinancistoProvider';
 import { extract, readFile } from './util';
 
-const NestedSets = require('nested-sets-tree');
-
-const tree = new NestedSets({
-    id: 'id',
-    lft: 'left',
-    rgt: 'right'
-});
-
 readFile(`${__dirname}/../test/data/20180503_100107_997.backup`)
     .then(function (file) {
         let data = extract(file);
